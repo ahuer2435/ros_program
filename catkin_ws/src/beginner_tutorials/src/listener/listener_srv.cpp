9de3f53b5d127978852a1241249cae28e8c215c3
ahuer <18221079843@139.com>
2019-12-01 12:14:05 +0800

    update ros srv.wq

--- catkin_ws/src/beginner_tutorials/src/listener/listener_srv.cpp
+++ catkin_ws/src/beginner_tutorials/src/listener/listener_srv.cpp
@@ -1,11 +1,11 @@
 #include "ros/ros.h"  //ros.h 包含ros程序常用的头文件。
-#include "beginner_tutorials/AddTwoInts.h"
+#include "beginner_tutorials/AddTwoInts.h"  //引用服务类型头文件
 
-//回调函数定义，参数类型需要与所订阅的消息类型保持一致
+//回调函数定义，参数类型需要与所服务类型保持一致
 bool add(beginner_tutorials::AddTwoInts::Request  &req,
          beginner_tutorials::AddTwoInts::Response &res)
 {
-  res.sum = req.a + req.b;
+  res.sum = req.a + req.b;      //更新响应值。
   ROS_INFO("request: x=%d, y=%d", req.a, req.b);
   ROS_INFO("sending back response: [%d]", res.sum);
   return true;
@@ -16,7 +16,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "listener_srv");  //初始化ros，并命令节点名。
   ros::NodeHandle n;        //初始化节点，调用ros api接口句柄。
 
+  //定义一个服务者，服务名字：add_two_ints，服务回调函数：add
   ros::ServiceServer service = n.advertiseService("add_two_ints", add);
-  ros::spin();  //不返回函数，监听订阅者中的回调队列，并执行回调函数。
+  ros::spin();  //不返回函数，监听服务节点中的回调队列，并执行回调函数。
   return 0;     //正常运行时，不会执行到。
 }
--- catkin_ws/src/beginner_tutorials/src/talker/talker_srv.cpp
+++ catkin_ws/src/beginner_tutorials/src/talker/talker_srv.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"    //ros.h 包含ros程序常用的头文件。
-#include "beginner_tutorials/AddTwoInts.h"
+#include "beginner_tutorials/AddTwoInts.h"  //引用服务类型头文件
 
 int main(int argc, char **argv)
 {
@@ -7,15 +7,22 @@ int main(int argc, char **argv)
   ros::NodeHandle n;    //初始化节点，调用ros api接口句柄。
   //定义一个客户端
   ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
-  beginner_tutorials::AddTwoInts srv;
-  srv.request.a = 100;
-  srv.request.b = 10;
-  if (client.call(srv)) {
-    ROS_INFO("Sum: %d", srv.response.sum);
-  }
-  else {
-    ROS_ERROR("Failed to call service add_two_ints");
-    return 1;
+  ros::Rate loop_rate(1);  //设置发布频率
+  int count = 0;    //发布请求计数
+  beginner_tutorials::AddTwoInts srv;   //定义服务变量
+  while (ros::ok()){    //检测节点是否正常运行
+    srv.request.a = count;      //更新服务变量值。
+    srv.request.b = count+1;
+    if (client.call(srv)) {     //发送请求
+      ROS_INFO("Sum: %d", srv.response.sum);  //收到请求响应。
+    }
+    else {
+      ROS_ERROR("Failed to call service add_two_ints"); //请求响应失败
+      return 1;
+    }
+    ros::spinOnce();    //处理回调函数，会返回，这里也可以不加，因为此节点没有回调函数。
+    loop_rate.sleep();  //与Line 10配套使用，用于控制发布频率。
+    ++count;            //消息计数。
   }
   return 0;
 }
