77ef777032158c79d64679c76cdf3b87f5f140d7
ahuer <18221079843@139.com>
2020-01-01 17:43:48 +0800

    ros plugin step2.

--- catkin_ws/src/beginner_tutorials/src/plugin_use/plugin_use.cpp
+++ catkin_ws/src/beginner_tutorials/src/plugin_use/plugin_use.cpp
@@ -1,28 +1,33 @@
 #include <ros/ros.h>
 #include <pluginlib/class_loader.h>
 #include <pluginlib_tutorials/polygon_base.h>
+#include <beginner_tutorials/Area.h>
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "plugin_use");  //初始化ros，并命令节点名。
     ros::NodeHandle n;        //初始化节点，调用ros api接口句柄。
+    beginner_tutorials::Area area;
+    ros::Publisher plugin_pub = n.advertise<beginner_tutorials::Area>("plugin_msg", 1);
 
     pluginlib::ClassLoader<polygon_base::RegularPolygon> poly_loader("pluginlib_tutorials", "polygon_base::RegularPolygon");
 
     try{
         boost::shared_ptr<polygon_base::RegularPolygon> triangle = poly_loader.createInstance("polygon_plugins::Triangle");
         triangle->initialize(10.0);
-
         boost::shared_ptr<polygon_base::RegularPolygon> square = poly_loader.createInstance("polygon_plugins::Square");
         square->initialize(10.0);
 
-        ROS_INFO("Triangle area: %.2f", triangle->area());
-        ROS_INFO("Square area: %.2f", square->area());
+        area.triangleArea = triangle->area();
+        area.squareAresa = square->area();
+
+        ROS_INFO("Triangle area: %.2f", area.triangleArea);
+        ROS_INFO("Square area: %.2f", area.squareAresa);
     }
     catch(pluginlib::PluginlibException& ex) {
         ROS_ERROR("The plugin failed to load for some reason. Error: %s", ex.what());
     }
-
+    plugin_pub.publish(area);
     ros::spin();
-  return 0;
+    return 0;
 }
