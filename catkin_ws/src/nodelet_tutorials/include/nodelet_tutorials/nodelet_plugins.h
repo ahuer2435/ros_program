39685a2fe739ecca72a282daee95c78f420105d8
ahuer <18221079843@139.com>
2020-01-15 21:32:42 +0800

    add nodelet example.

--- catkin_ws/src/nodelet_tutorials/include/nodelet_tutorials/nodelet_plugins.h
+++ catkin_ws/src/nodelet_tutorials/include/nodelet_tutorials/nodelet_plugins.h
@@ -2,25 +2,25 @@
 #define NODELET_PLUGINS_H_
 
 #include <ros/ros.h>
-#include <nodelet/nodelet.h>
+#include <nodelet/nodelet.h>      // 基类Nodelet所在的头文件。
 #include <std_msgs/Float64.h>
 
 namespace nodelet_tutorials
 {
-class nodeletTalker : public nodelet::Nodelet
+class nodeletTalker : public nodelet::Nodelet   //任何nodelet plugin都要继承Nodelet类。
 {
 public:
   nodeletTalker()
   {}
 
 private:
-  virtual void onInit()
+  virtual void onInit()     //此函数声明部分为固定格式，在nodelet加载此plugin会自动执行此函数。
   {
-    ros::NodeHandle& private_nh = getPrivateNodeHandle();
+    ros::NodeHandle& private_nh = getPrivateNodeHandle();   //getPrivateNodeHandle()获取plugin节点名，用于命名其话题以及参数命名空间。
     pub_ = private_nh.advertise<std_msgs::Float64>("out", 1,true);
     std_msgs::Float64Ptr output(new std_msgs::Float64());
     output->data = 10;
-    NODELET_DEBUG("publish data %f", output->data);
+    NODELET_INFO("publish data %f", output->data);
     pub_.publish(output);    
   }
 
--- catkin_ws/src/nodelet_tutorials/src/nodelet_plugins.cpp
+++ catkin_ws/src/nodelet_tutorials/src/nodelet_plugins.cpp
@@ -2,4 +2,5 @@
 #include <nodelet/nodelet.h>
 #include <nodelet_tutorials/nodelet_plugins.h>
 
+//PLUGINLIB_EXPORT_CLASS()将C++类制作为为ros plugin。
 PLUGINLIB_EXPORT_CLASS(nodelet_tutorials::nodeletTalker, nodelet::Nodelet)
